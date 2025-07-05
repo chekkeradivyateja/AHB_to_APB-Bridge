b6f3fceb2ba16ba4080b085b9b82cfe98f63d5c8
Divya Teja Chekkera <chakkeradivyatejareddy@gmail.com>
2025-07-05 21:58:05 +0530

    Update AHB_Slave_Interface.v

--- AHB_Slave_Interface.v
+++ AHB_Slave_Interface.v
@@ -1,10 +1,9 @@
 
-// AHB to APG Bridge | Maven Silicon
+// AHB to APG Bridge
 //
 //
 //
 // AHB Slave Interface
-// Date:04-06-2022
 //
 //
 // Modifications: The Combinational part sensitivity list did not inclued Hresetn and hence they gave x output on reset
